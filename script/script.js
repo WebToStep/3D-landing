6136f38ab36110028d04200467a94528577e3c58
WebStep <webtostep@yandex.kz>
2021-08-27 09:56:25 +0500

    les24start

--- script/script.js
+++ script/script.js
@@ -296,4 +296,36 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     };
     inputsHandler();
+    // calculator
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.querySelector('.calc-total');
+
+        const countSum = () => {
+            let total = 0;
+            let typeValue = calcType.getAttribute('value');
+            console.log('typeValue: ', typeValue);
+            let squareValue;
+
+
+
+
+            totalValue.textContent = total;
+        };
+        countSum();
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')) {
+                console.log('ku');
+            }
+        });
+
+    };
+    calc(100);
 });
