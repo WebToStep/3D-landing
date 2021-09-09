9c676ea769e3896ca7692a7b3372d5895cc3f3c6
WebStep <webtostep@yandex.kz>
2021-09-09 11:58:35 +0500

    слайдер карусель первый комит

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import command from './modules/command';
 import calc from './modules/calc';
 import sendForm from './modules/sendForm';
 import { validForm1, validForm2, validForm3 } from './modules/validators';
+import SliderCarousel from './modules/sliderCarousel';
 
 // Timer
 // if new client set localStorage timer
@@ -32,8 +33,29 @@ slider();
 command();
 // calculator
 calc(100);
-// validators
 
+// carousel
+const carousel = new SliderCarousel({
+    main: '.companies-wrapper',
+    wrap: '.companies-hor',
+    slidesToShow: 4,
+    infinity: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            slidesToShow: 3
+        },
+        {
+            breakpoint: 768,
+            slidesToShow: 2
+        }, {
+            breakpoint: 576,
+            slidesToShow: 1
+        }],
+});
+carousel.init();
+
+// validators
 validForm1.init();
 validForm2.init();
 validForm3.init();
